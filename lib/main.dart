0eb8e8741fa03ae249a56370b244d35c3048a980
vivekismittal <mittalvivek700@gmail.com>
2022-03-03 00:17:24 +0530

    secondCommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/product_details_screen.dart';
+import './screens/cart_screen.dart';
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 import './providers/products.dart';
-import './screens/product_overview-screen.dart';
+import './providers/cart.dart';
+import './providers/orders.dart';
+import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-          colorScheme: ColorScheme.dark(
-            primary: Colors.purple,
-            primaryVariant: Colors.deepPurple,
-            secondary: Colors.orange,
-            secondaryVariant: Colors.deepOrange,
-            surface: Colors.pinkAccent,
-            background: Colors.amber,
-            error: Colors.red,
-            // onPrimary: Colors.deepOrange,
-            // onSecondary: Colors.deepOrange,
-            // onSurface: Colors.deepOrange,
-            // onBackground: Colors.deepOrange,
-            // onError: Colors.deepOrange,
-            brightness: Brightness.dark,
-          ),
-          // fontFamily: 'Lato',
+    return MultiProvider(
+      providers: [
+        Provider(
+          create:(_)=> Products(),
         ),
-        home: ProductOverviewScreen(),
-        routes: {
-          ProductDetailsScreen.routeName: (context) => ProductDetailsScreen(),
-        },
-      ),
+         Provider(
+          create:(_)=> Cart(),
+        ),
+         Provider(
+          create:(_)=> Orders(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            accentColor: Colors.deepOrange,
+            primaryColor: Colors.purple,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          }),
     );
   }
 }
